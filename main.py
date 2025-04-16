69b4d090a9bf12578c341ea78a3bb22b6f1ebfc4
nica <seowieww@gmail.com>
2025-04-16 21:16:16 +0600

    дз №3

--- main.py
+++ main.py
@@ -2,26 +2,74 @@
 import flet as ft
 
 def main(page: ft.Page):
-    friends_list = []
+    total_sum = 0
 
-    def click_button(e):
-        if name_input.value.strip():
-            friends_list.append(name_input.value)
-            print(friends_list)
-            name_input.value = ""
+    page.title = "расходы"
+    page.bgcolor = ft.colors.PINK_50
+    page.theme_mode = ft.ThemeMode.LIGHT
+
+    name_input = ft.TextField(label="описание расхода")
+    amount_input = ft.TextField(label="сумма", keyboard_type="number")
+
+    expenses_list = ft.Column()
+
+    total_text = ft.Text("0", size=20, weight="bold", color=ft.colors.BLACK)
+
+    def add_expense(e):
+        nonlocal total_sum
+
+        name = name_input.value.strip()
+        amount_str = amount_input.value.strip()
+
+        try:
+            amount = float(amount_str)
+            if amount <= 0:
+                raise ValueError
+        except ValueError:
+            error_text = ft.Text("введите положительное число!")
+            page.add(error_text)
             page.update()
+            return
 
-    name_input = ft.TextField(
-        label="Введите имя друга",
-    )
+        if amount <= 100:
+            color = ft.colors.LIGHT_GREEN_500
+        elif amount <= 1000:
+            color = ft.colors.LIGHT_BLUE_200  
+        elif amount <= 10000:
+            color = ft.colors.PURPLE_300
+        else:
+            color = ft.colors.BLACK  
+
+        expenses_list.controls.append(
+            ft.Row([
+                ft.Text(name, color=ft.colors.BLACK),
+                ft.Text(f"{amount:.2f}", color=color)
+            ])
+        )
+
+        total_sum += amount
+        total_text.value = f"{total_sum:.2f}"
+
+        name_input.value = ""
+        amount_input.value = ""
+
+        page.update()
 
     button = ft.ElevatedButton(
-        text="Сохранить",
-        on_click=click_button,
-        color=ft.colors.PINK,
-        bgcolor=ft.colors.AMBER,
+        text="добавить расход",
+        on_click=add_expense,
+        bgcolor=ft.colors.PINK_300,
+        color=ft.colors.WHITE
     )
 
-    page.add(name_input, button)
+    page.add(
+        name_input,
+        amount_input,
+        button,
+        ft.Text("список расходов:", size=18, weight="bold", color=ft.colors.BLACK),
+        expenses_list,
+        ft.Text("общая сумма:", size=18, weight="bold", color=ft.colors.BLACK),
+        total_text
+    )
 
 ft.app(target=main)
