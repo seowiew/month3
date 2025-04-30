7eec65a8b4f208b0a0fbc562e3b263b8c6694da6
nica <seowieww@gmail.com>
2025-04-30 16:20:33 +0600

    дз №7

--- src/main.py
+++ src/main.py
@@ -6,12 +6,12 @@ db = Database("expenses.db")
 
 
 def main(page: ft.Page):
-    page.title = "расходы"
+    page.title = "Расходы"
     page.bgcolor = ft.colors.PINK_50
     page.theme_mode = ft.ThemeMode.LIGHT
 
-    name_input = ft.TextField(label="описание расхода")
-    amount_input = ft.TextField(label="сумма", keyboard_type="number")
+    name_input = ft.TextField(label="Описание расхода")
+    amount_input = ft.TextField(label="Сумма", keyboard_type="number")
     expenses_list = ft.Column(scroll="always", expand=True)
     total_text = ft.Text(str(db.get_total()), size=20, weight="bold", color=ft.colors.BLACK)
 
@@ -28,6 +28,18 @@ def main(page: ft.Page):
                 ft.Row([
                     ft.Text(name, color=ft.colors.BLACK),
                     ft.Text(f"{amount:.2f}", color=color),
+                    ft.IconButton(
+                        icon=ft.icons.EDIT_OUTLINED,
+                        icon_color=ft.colors.BLUE,
+                        data=name,
+                        on_click=open_edit_modal  # кнопка для редактирования
+                    ),
+                    ft.IconButton(
+                        icon=ft.icons.DELETE,
+                        icon_color=ft.colors.RED,
+                        data=name,
+                        on_click=open_delete_modal
+                    )
                 ])
             )
 
@@ -40,7 +52,7 @@ def main(page: ft.Page):
             if amount <= 0:
                 raise ValueError
         except ValueError:
-            page.add(ft.Text("введите положительное число!", color=ft.colors.RED))
+            page.add(ft.Text("Введите положительное число!", color=ft.colors.RED))
             page.update()
             return
 
@@ -53,17 +65,76 @@ def main(page: ft.Page):
         total_text.value = f"{db.get_total():.2f}"
         page.update()
 
+    def open_delete_modal(e):
+        page.data = e.control.data
+        page.open(delete_modal)
+
+    def close_delete_modal(e):
+        page.close(delete_modal)
+
+    def delete_expense(e):
+        db.delete_expense(page.data)
+        page.close(delete_modal)
+
+        load_expenses()
+        total_text.value = f"{db.get_total():.2f}"
+        page.update()
+
+    def open_edit_modal(e):
+        name_input.value = e.control.data  # заполняем поле редактирования
+        page.open(update_modal)
+
+    def close_edit_modal(e):
+        page.close(update_modal)
+
     button = ft.ElevatedButton(
-        text="добавить расход", on_click=add_expense,
+        text="Добавить расход", on_click=add_expense,
         bgcolor=ft.colors.PINK_300, color=ft.colors.WHITE
     )
 
     input_row = ft.Row(controls=[name_input, amount_input, button])
 
-    load_expenses()  # показать существующие
+    load_expenses()
+
+    delete_modal = ft.AlertDialog(
+        modal=True,
+        title=ft.Text("Подтвердите удаление"),
+        content=ft.Text("Вы действительно хотите удалить этот расход?"),
+        actions=[
+            ft.ElevatedButton(
+                "Удалить",
+                on_click=delete_expense,
+                bgcolor=ft.Colors.RED,
+                color=ft.Colors.WHITE,
+            ),
+            ft.ElevatedButton("Отменить", on_click=close_delete_modal),
+        ],
+        actions_alignment=ft.MainAxisAlignment.END,
+    )
+
+    update_modal = ft.AlertDialog(
+        modal=True,
+        title=ft.Text("Редактирование расхода"),
+        content=ft.Column(
+            controls=[
+                name_input,
+                amount_input,
+            ]
+        ),
+        actions=[
+            ft.ElevatedButton(
+                "Сохранить",
+                on_click=add_expense,
+                bgcolor=ft.Colors.BLUE,
+                color=ft.Colors.WHITE,
+            ),
+            ft.ElevatedButton("Отменить", on_click=close_edit_modal),
+        ],
+    )
 
     page.add(
-        ft.Text("Ваши расходы", size=30, weight=ft.FontWeight.BOLD, color=ft.colors.BLACK),
+        ft.Text("Ваши расходы", size=30, weight=ft.
+                FontWeight.BOLD, color=ft.colors.BLACK),
         input_row,
         ft.Text("Общая сумма расходов:", size=18, weight="bold", color=ft.colors.BLACK),
         total_text,
